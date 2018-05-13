7f63d06747e51c18aa0d71a13c9f14be5504989d
Ryoichi Gokan <nargok@gmail.com>
2018-05-13 10:30:47 +0900

    checkUser function

--- functions/index.js
+++ functions/index.js
@@ -12,3 +12,38 @@ app.use(cors);
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// 仕様
+// 認証しなくてもチャット機能を使える
+
+// ユーザ情報の取得
+const anonymousUser = {
+  id: "anon",
+  name: "Anonymous",
+  avatar: ""
+};
+
+const checkUser = (req, res, next) => {
+  req.user = anonymousUser;
+  if (req.query.auth_token != undefined) {
+    let idToken = req.query.auth_token;
+    // ユーザを検証する
+    admin.auth().verifyIdToken(idToken).then(decodedIdtoken => {
+      // 検証したユーザの情報を設定する
+      let authUser = {
+        id :decodedIdtoken.user_id,
+        name: decodedIdtoken.name,
+        avatar: decodedIdtoken.picture
+      };
+      // reqオブジェクトにuserとして設定する
+      req.user = authUser;
+      next();
+    }).catch(error => {
+      next();
+    });
+  } else {
+    next();
+  };
+};
+
+app.use(checkUser);
