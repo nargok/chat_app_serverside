6860edf0ef421b07ac7a62899142eabe812f24cc
Ryoichi Gokan <nargok@gmail.com>
2018-05-13 10:21:15 +0900

    Inital setting for firebase & express

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,10 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const express = require('express');
+const app = express();
+const cors = require('cors')({origin: true})
+app.use(cors);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
